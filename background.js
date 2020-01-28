5ef5776ee1ea627ac0e5f7eb4cfc00754963ddc1
Adam <advation@users.noreply.github.com>
2020-01-28 15:55:13 -0700

    Initial commit

---
+++ background.js
@@ -0,0 +1,30 @@
+var allowed_domain = "2020census.gov";
+var redirect_url = "http://2020census.gov";
+
+function redirect(requestDetails) {
+	request_url = requestDetails.url;
+
+	var regex = new RegExp("^(?:http(?:s)?:\/\/)?(?:[^\.]+\.)?"+allowed_domain+"(/.*)?$");
+
+	// Check if within the same domain
+	if(request_url.match(regex)) {
+		// If within the domain
+		console.log('Request passed')
+	}
+	else
+	{
+		// If prohibited domain
+		console.log('Prohibitied Domain: ' + request_url)
+		return {
+			redirectUrl:  browser.runtime.getURL(redirect_url)
+		}
+	}
+}
+
+
+// Intercept all requests
+browser.webRequest.onBeforeSendHeaders.addListener (
+  redirect,
+  {urls:["<all_urls>"],types:["main_frame"]},
+  ["blocking"]
+);
